dba01b8b2aa4181d5ace394bba4bd9966c7d9747
Edobayupamungkas <Edobayupamungkas545@gmail.com>
2019-09-12 23:30:41 +0700

    TugasPP

---
+++ Tugas PP/Edobayupamungkas_H071171004/Senorita4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Senorita4 {
+  public static void main (String [] args) {
+    Scanner obj = new Scanner(System.in);
+    int N = obj.nextInt();
+
+    if ( N % 2 == 0 ) {
+      if ( N >= 10 );
+      if ( N < 100); {
+        System.out.println ( N + "Bilangan Genap lebih atau sama dengan 10 kurang dari 100");
+      }
+    }else {
+      if ( N >= 10 );
+      if ( N < 100); {
+        System.out.println ( N + "Bilangan Genap lebih atau sama dengan 10 kurang dari 100");
+      }
+    }
+  }
+}
---
+++ Tugas PP/Edobayupamungkas_H071171004/Senorita6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Senorita6 {
+  public static void main (String [] args) {
+    Scanner obj = new Scanner(System.in);
+    int X = obj.nextInt();
+
+    if ( X > 0 ) {
+      System.out.println ( X + " adalah Bilangan Positif ");
+    }else
+      if (X < 0) {
+      System.out.println ( X + " adalah Bilangan Negatif ");
+    }else 
+      if (X == 0) {
+      System.out.println ( X + " adalah Bilangan 0 ");
+    }
+  }
+}
